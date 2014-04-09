61133a4523b04db58bc7a1e8fcd8b772a40a5215
fnutrica <fnutrica@macalester.edu>
2014-04-08 22:21:36 -0500

    MyLinkedList completed

--- src/org/macalester/edu/comp124/lists/MyLinkedList.java
+++ src/org/macalester/edu/comp124/lists/MyLinkedList.java
@@ -10,101 +10,104 @@ package org.macalester.edu.comp124.lists;
  * @param <E>
  */
 public class MyLinkedList<E> {
-	private MyLinkedNode<E> head;
-	private MyLinkedNode<E> tail;
-	private int numElements = 0;
+    private MyLinkedNode<E> head;
+    private MyLinkedNode<E> tail;
+    private int numElements = 0;
 
     /**
      * Creates a new empty linked list.
      */
-	public MyLinkedList() {
-		head = new MyLinkedNode<E>(null);
-		tail = new MyLinkedNode<E>(null);
-		clear();
-	}
+    public MyLinkedList() {
+        head = new MyLinkedNode<E>(null);
+        tail = new MyLinkedNode<E>(null);
+        clear();
+    }
 
     /**
      * Returns the element at position index.
      * @param index
      * @return
      */
-	public E get(int index) {
+    public E get(int index) {
         MyLinkedNode<E> current= head;
         int n=index+1;
         while(n>0){
 
-        current=current.getNext();
-        n--;
+            current=current.getNext();
+            n--;
 
         }
-		return current.getValue();
-	}
+        return current.getValue();
+    }
 
     /**
      * Adds a new element to the end of the list:
      *
      * @param elem
      */
-	public void add(E elem) {
+    public void add(E elem) {
 
         MyLinkedNode<E> last = tail.getPrev();
         tail.setPrev(new MyLinkedNode<E>(elem));
         last.setNext(tail.getPrev());
         numElements++;
 
-	}
+    }
 
     /**
      * Inserts a new element at the specified index.
      *
      * @param elem
      */
-	public void add(int i, E elem) {
+    public void add(int i, E elem) {
         int m=i;
-        MyLinkedNode<E> copyp= head;
-        while (m>1){
+        MyLinkedNode<E> copyp= head.getNext();
+        while (m!=1){
             copyp=copyp.getNext();
             m--;
         }
-if ( m<numElements-1){
-        MyLinkedNode<E> copyd= copyp.getNext();
-    copyp.setNext(new MyLinkedNode<E>(elem));
-    copyd.setPrev(copyp.getNext());
 
-}
+        if ( i<numElements){
+            MyLinkedNode<E> copyd= copyp.getNext();
+            System.out.println(copyd.getValue());
+            copyp.setNext(new MyLinkedNode<E>(elem));
+            copyp.getNext().setNext(copyd);
+            //copyd.setPrev(copyp.getNext());
+
+        }
         else  {
-    MyLinkedNode<E> copyd=  tail.getPrev();
-    tail.setPrev(new MyLinkedNode<E>(elem));
-    copyd.setNext(tail.getPrev());
+            MyLinkedNode<E> copyd=  tail;
+            tail.setPrev(new MyLinkedNode<E>(elem));
+            copyd.setNext(tail.getPrev());
 
-}
+        }
 
         numElements++;
 
 
-	}
+    }
 
     /**
      * Returns the current size of the list.
      * @return
      */
-	public int size() {
-		return numElements;
-	}
+    public int size() {
+        return numElements;
+    }
 
     /**
      * Clears the contents of the list.
      */
-	public void clear() {
-		// set head's next to the tail
-		head.setNext(tail);
-		head.setPrev(null);
-		
-		// set tails prev to the head
-		tail.setPrev(head);
-		tail.setNext(null);
-		numElements = 0;
-	}
+    public void clear() {
+        // set head's next to the tail
+        head.setNext(tail);
+        head.setPrev(null);
+
+        // set tails prev to the head
+        tail.setPrev(head);
+        tail.setNext(null);
+        numElements = 0;
+    }
 
 
     /**
